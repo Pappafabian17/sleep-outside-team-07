91e4bf81dde99df7aa751494b8221f54cccbb4ea
Fabian Pappa <fabian.pappa.ext@errepar.com>
2025-09-10 22:36:05 -0300

    deleting clg

--- src/js/product.js
+++ src/js/product.js
@@ -5,6 +5,4 @@ import ProductDetails from "./ProductDetails.mjs";
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
 const product = new ProductDetails(productId, dataSource);
-console.log("product", product);
 product.init();
-// console.log(dataSource.findProductById(productId));

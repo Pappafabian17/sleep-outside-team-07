2a2095302b09b34a97d51327fc4afbfa5b59207b
Fabian Pappa <fabian.pappa.ext@errepar.com>
2025-09-10 22:34:56 -0300

    week-02-team-group-actv

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,10 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const productCart = getLocalStorage("so-cart") || [];
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  productCart.push(product);
-  addProductToCart(productCart);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productId = getParam("product");
+const product = new ProductDetails(productId, dataSource);
+console.log("product", product);
+product.init();
+// console.log(dataSource.findProductById(productId));

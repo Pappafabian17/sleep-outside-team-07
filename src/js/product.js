613352842188424793272c0f9776b0b71c1a6a98
Fabian Pappa <fabian.pappa.ext@errepar.com>
2025-09-19 17:43:29 -0300

    developing w3

--- src/js/product.js
+++ src/js/product.js
@@ -1,37 +1,42 @@
-import { getParam, setLocalStorage, getLocalStorage, updateCartCount } from "./utils.mjs";
+import {
+  getParam,
+  setLocalStorage,
+  getLocalStorage,
+  updateCartCount,
+} from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 const productID = getParam("product");
 
-const product = new ProductDetails(productID, dataSource);
-product.init();
+const currentProduct = new ProductDetails(productID, dataSource);
+currentProduct.init();
 
-function addProductToCart(product) {
+function addProductToCart(productItem) {
   // Get existing cart or create empty array
   const existingCart = getLocalStorage("so-cart") || [];
-  
+
   // Add new product
-  existingCart.push(product);
-  
+  existingCart.push(productItem);
+
   // Save updated cart
   setLocalStorage("so-cart", existingCart);
-  
+
   // Update cart count display
   updateCartCount();
 }
 
 // add to cart button event handler
 async function addToCartHandler(e) {
-    const product = await dataSource.findProductById(e.target.dataset.id);
-    addProductToCart(product);
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
 }
 
 // add listener to Add to Cart button
 document
-    .getElementById("addToCart")
-    .addEventListener("click", addToCartHandler);
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
 
 // Initialize cart count when this module loads
 updateCartCount();
